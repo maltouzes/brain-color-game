1cc5712c23aa075d394d58b493158a459fa386f2
maltouzes <maltouzes@gmail.com>
2015-10-01 12:50:15 +0200

    Lots of improvements  - Code more documented  - restart method (reset points and welcome phrases)

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import random
 
 class BoxLayoutGame(BoxLayout):
     """ BoxLayout called by kivy """
-
+# Text when the game start
     text = 'Push a button for start'
 
 # Color: red, green, blue and yellow
@@ -26,21 +26,22 @@ class BoxLayoutGame(BoxLayout):
     texts = [text1, text2, text3, text4]
 
     texts_test = ""
+# Used for mix colors and texts
     number_random = 0
 # Used for count points
     points = 0
     no_points = 0
     points_str = ""
-
+# Choose the game mode, by default: Colours Mode
     mode_game = "Colours Mode"
 
     def start(self):
         """ on_click start this method """
-        mode_game_kv = self.ids['spinner_game']
-        self.mode_game = mode_game_kv.text
+# Change the game mode (Colors Mode or Text Mode)
+# Mix colors and texts
         random.shuffle(self.colors)
         random.shuffle(self.texts)
-
+# Kivy buttons text
         color1 = self.ids['bt1']
         color1.color = self.colors[0]
         color1.text = self.texts[0]
@@ -60,15 +61,19 @@ class BoxLayoutGame(BoxLayout):
         self.number_random = random.randint(0, 3)
         self.text = 'Push the button ' +\
                     str(self.texts[self.number_random])
+        self.restart()
         welcome.text = self.text
 
     def count_points(self, nbr):
         """ Count the points """
         points_kv = self.ids['points']
+# Start the Game now
         if self.points_str == "":
             self.points_str = " "
         else:
+            # Game already started: Count points (depends on the Game mode)
             if self.mode_game == "Colours Mode":
+                # Colours Mode
                 self.color_name_to_rgb(self.texts[self.number_random])
                 if self.colors[nbr] == self.texts_test:
                     self.points += 1
@@ -77,6 +82,7 @@ class BoxLayoutGame(BoxLayout):
                 points_kv.text = "Points " + str(self.points) +\
                                  "   Miss " + str(self.no_points)
             elif self.mode_game == "Text Mode":
+                # Text Mode
                 if nbr == self.number_random:
                     self.points += 1
                 else:
@@ -86,8 +92,26 @@ class BoxLayoutGame(BoxLayout):
             else:
                 pass
 
+    def restart(self):
+        mode_game_kv = self.ids['spinner_game']
+        self.points_kv = self.ids['points']
+        if mode_game_kv.text != self.mode_game:
+            self.points = 0
+            self.no_points = -1
+            self.points_kv.text = "Push a button for start"
+            if self.mode_game == "Colours Mode":
+                self.text = "Start Text Mode"
+            elif self.mode_game == "Text Mode":
+                self.text = "Start Colours Mode"
+            else:
+                pass
+            self.mode_game = mode_game_kv.text
+        else:
+            pass
+
     def color_name_to_rgb(self, name):
         """ Change a name color to a rgb color """
+# Replace Webcolors library
         if name == 'red':
             name = [1, 0, 0, 1]
         elif name == 'green':
